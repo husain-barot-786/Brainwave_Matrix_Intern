a7b5ac66740b65c19d264dff55242ca1898b3c02
Husain Barot <husain.barot.2002@gmail.com>
2025-07-10 22:51:03 +0530

    Task 1 - Python-ATM-Interface

--- Task 2 - Inventory Management System/auth.py
+++
@@ -1,7 +0,0 @@
-from user import User, UserManager
-
-user_manager = UserManager()
-user_manager.add_user(User("admin", "admin123"))  # Default admin
-
-def login(username, password):
-    return user_manager.authenticate(username, password)
